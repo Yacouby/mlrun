29f81ec8ba79891c86eb0f5a08b53799d8d5edf5
yacouby <yacouby@iguazio.com>
2023-11-16 14:57:17 +0200

    aaaa

--- mlrun/runtimes/base.py
+++ mlrun/runtimes/base.py
@@ -196,6 +196,9 @@ class BaseRuntime(ModelObj):
         self.metadata.labels[key] = str(value)
         return self
 
+    def set_categories(self, categories: List[str]):
+        self.metadata.categories = categories
+
     @property
     def uri(self):
         return self._function_uri()
